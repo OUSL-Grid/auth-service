3484f661f0918dde4b533495977cc841347a29df
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-08-15 14:20:25 +0530

    create a UserRepository class

--- src/db/repository.py
+++ src/db/repository.py
@@ -3,58 +3,61 @@ from uuid import uuid4
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from src.auth import hash_password
-from src.db.models import Users
+from src.db.models import Users as UserModel
 from src.db.schemas import UserCreate, UserRoleUpdate
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class UserRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def create_user(self, payload: UserCreate) -> UserModel:
+        """Create a new user using payload"""
+        user_data = payload.model_dump()
+
+        user_data["id"] = str(uuid4())
+
+        new_user = UserModel(**user_data)
+
+        self.db.add(new_user)
+        self.db.commit()
+        self.db.refresh(new_user)
+
+        return new_user
 
 
-def create_user(session: Session, payload: UserCreate) -> Users:
-    """Create a new user using payload"""
-    user_data = payload.model_dump()
-
-    user_data["id"] = str(uuid4())
-
-    new_user = Users(**user_data)
-
-    session.add(new_user)
-    session.commit()
-    session.refresh(new_user)
-
-    return new_user
+    def get_user_by_email(self, email: str) -> UserModel:
+        """Fetch user by email"""
+        return self.db.execute(
+            select(UserCreate).where(UserCreate.email == email)
+        ).scalar_one_or_none()
 
 
-def get_user_by_email(session: Session, email: str) -> Users:
-    """Fetch user by email"""
-    return session.execute(
-        select(Users).where(Users.email == email)
-    ).scalar_one_or_none()
+    def get_user_by_id(self, id: str) -> UserModel:
+        """Fetch user by email"""
+        return self.db.execute(select(UserModel).where(UserModel.id == id)).scalar_one_or_none()
 
 
-def get_user_by_id(session: Session, id: str) -> Users:
-    """Fetch user by email"""
-    return session.execute(select(Users).where(Users.id == id)).scalar_one_or_none()
+    def update_role(session: Session, payload: UserRoleUpdate) -> UserModel:
+        user = session.get(UserModel, payload.id)
+        if not user:
+            return None
+
+        update_data = payload.model_dump(exclude_unset=True)
+
+        for key, value in update_data.items():
+            setattr(user, key, value)
+
+        session.commit()
+        session.refresh(user)
+        return user
 
 
-def update_role(session: Session, payload: UserRoleUpdate) -> Users:
-    user = session.get(Users, payload.id)
-    if not user:
-        return None
-
-    update_data = payload.model_dump(exclude_unset=True)
-
-    for key, value in update_data.items():
-        setattr(user, key, value)
-
-    session.commit()
-    session.refresh(user)
-    return user
-
-
-def delete_user_by_id(session: Session, id: str) -> bool:
-    """delete user from database"""
-    user = session.get(Users, id)
-    if not user:
-        return False
-    session.delete(user)
-    session.commit()
-    return True
+    def delete_user_by_id(session: Session, id: str) -> bool:
+        """delete user from database"""
+        user = session.get(UserModel, id)
+        if not user:
+            return False
+        session.delete(user)
+        session.commit()
+        return True
